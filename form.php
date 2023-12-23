c6ff5ac6eb9576e2d81aafc0e9392589fcf34438
sambathmarchi <sambathmarchi24@gmail.com>
2023-12-23 17:19:04 +0100

    Files Created

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+
+<body>
+    <form action="password.php">
+        <input type="text" placeholder="Lunghezza Password" name="passwordLength" required>
+        <button type="submit">Invia</button>
+    </form>
+</body>
+
+</html>
